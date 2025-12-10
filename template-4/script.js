406d5f9fbf9e594851c2c0b0cc08bd7e1daa02a3
ChandraguptSharma07 <chandragupt.jsr@gmail.com>
2025-12-10 23:57:31 +0530

    made a few more templates

---
+++ template-4/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const navButtons = document.querySelector('.nav-buttons');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+        navButtons.classList.toggle('active');
+    });
+});
